f58c81d81c3cba25e1e94cdf22dbadd97a1a35c4
Johnny Briones <jbrrii91@gmail.com>
2021-01-24 09:39:20 -0500

    Decreased font size for h4

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,16 +8,16 @@ function HomePage() {
     <div style={{ marginTop: '0px'}}>
         <NavigationBar />
       <h1>Fantasy Stock Application</h1>
-      <h4 style={{ wordWrap: "break-word" }}>
+      <h4 style={{ marginTop: '30px', wordWrap: "break-word", fontSize: "16px" }}>
         Welcome to where you can practice how to invest in the stock market
-        before you risk your hard earned money.
+        before you risk your hard earned cash.
       </h4>
       <img
-        style={{ borderRadius: "50%", margin: '100px' }}
+        style={{ borderRadius: "50%", margin: '40px' }}
         src={homepageCoinImage}
         alt="Homepage Coins"
       />
-      <h5>
+      <h5 style={{ marginTop: "30px" }}>
         “Remember that the stock market is a manic depressive.” - Warren Buffet
       </h5>
     </div>
