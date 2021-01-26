3dedeefbb8925fa28a970f2d95e173894a9778d5
Johnny Briones <jbrrii91@gmail.com>
2021-01-26 01:07:37 -0500

    Search bar in Summary page

--- src/components/SummaryPage.js
+++ src/components/SummaryPage.js
@@ -3,12 +3,15 @@ import StockCard from './StockCard'
 
 function SummaryPage() {
     return(
+        <div>
+            <input placeholder='Search' />
         <div style ={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
             <StockCard />
             <StockCard />
             <StockCard />
             <StockCard />
             <StockCard />
+        </div>
         </div>
     )
 }
