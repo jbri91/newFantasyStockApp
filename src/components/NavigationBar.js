02b29993d306310059038af361864368aeeecf16
Johnny Briones <jbrrii91@gmail.com>
2021-02-07 12:33:28 -0500

    working on modal

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import bullMarketIcon from "../images/bullMarketIcon.png";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Button } from "bootstrap";
 
 function NavigationBar() {
   return (
@@ -13,22 +14,34 @@ function NavigationBar() {
           <img src={bullMarketIcon} alt="Bull Market" />{" "}
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
-          <NavLink style={{ color: "gray" }} to="/" href="home">
-            Home|
-          </NavLink>
-          <NavLink style={{ color: "gray" }} to="/report" href="reportPage">
-           Report|  
-          </NavLink>
-          <NavLink style={{ color: "gray" }} to="/summary" href="summaryPage">
-            Summary
-          </NavLink>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <NavLink style={{ color: "gray" }} to="/" href="home">
+              Home|
+            </NavLink>
+            <NavLink style={{ color: "gray" }} to="/report" href="reportPage">
+              Report|
+            </NavLink>
+            <NavLink style={{ color: "gray" }} to="/summary" href="summaryPage">
+              Summary
+            </NavLink>
           </div>
         </Nav>
-        <NavLink to='/login' style={{ color: "gray" }} href="#login">
+        <button
+          style={{ color: "black" }}
+          className="btn btn-info btn-lg"
+          data-toggle="modal"
+          data-target="#myModal"
+          href="#login"
+        >
           {" "}
           Login/Register{" "}
-        </NavLink>
+        </button>
       </Navbar>
     </div>
   );
