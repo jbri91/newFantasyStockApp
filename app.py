690f35b3869fdf2578607af2b07ff50790dbad78
Johnny Briones <jbrrii91@gmail.com>
2021-02-24 22:05:43 -0500

    can request data from test token from teh IEXCloud

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 import requests
 from flask_restful import Api, Resource, reqparse
 
+# Tpk_b6429f1574564a01b54d614f88e0f93f
+
 app = Flask(__name__)
 api = Api(app)
 
-response = requests.get('https://swapi.dev/api/people/1/')
-print(response.status_code)
+response = requests.get('https://sandbox.iexapis.com/stable/stock/IBM/quote?token=Tpk_b6429f1574564a01b54d614f88e0f93f')
+print(response.json())
 
 # @app.route('/hello')
 # def hello_world():
