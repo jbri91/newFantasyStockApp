73592c10dda22a4af0e46cb7123140ea99571bbc
Johnny Briones <jbrrii91@gmail.com>
2021-03-14 19:22:40 -0400

    deleted comments in app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 import requests
 from flask_restful import Api, Resource, reqparse
 
-# Tpk_b6429f1574564a01b54d614f88e0f93f
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,11 +9,6 @@ api = Api(app)
 stock = requests.get('https://sandbox.iexapis.com/stable/stock/IBM/quote?token=Tpk_b6429f1574564a01b54d614f88e0f93f').json()
 
 
-# @app.route('/hello')
- 
-# def hello_world():
-#     return 'Hello World'
-    
 class Stock(Resource):
     def get(self):
         return jsonify(stock)
