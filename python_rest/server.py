519af1648d65ca2e5d2628e74934a90b390515fb
Johnny Briones <jbrrii91@gmail.com>
2021-03-18 23:03:06 -0400

    commented out post and added debug at end

--- python_rest/server.py
+++ python_rest/server.py
@@ -13,32 +13,32 @@ class Employees(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        return {'employees': [i[5] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
     
-    def post(self):
-        conn = db_connect.connect()
-        print(request.json)
-        LastName = request.json['LastName']
-        FirstName = request.json['FirstName']
-        Title = request.json['Title']
-        ReportsTo = request.json['ReportsTo']
-        BirthDate = request.json['BirthDate']
-        HireDate = request.json['HireDate']
-        Address = request.json['Address']
-        City = request.json['City']
-        State = request.json['State']
-        Country = request.json['Country']
-        PostalCode = request.json['PostalCode']
-        Phone = request.json['Phone']
-        Fax = request.json['Fax']
-        Email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(LastName,FirstName,Title,
-                             ReportsTo, BirthDate, HireDate, Address,
-                             City, State, Country, PostalCode, Phone, Fax,
-                             Email))
-        return {'status':'success'}
+    # def post(self):
+    #     conn = db_connect.connect()
+    #     print(request.json)
+    #     LastName = request.json['LastName']
+    #     FirstName = request.json['FirstName']
+    #     Title = request.json['Title']
+    #     ReportsTo = request.json['ReportsTo']
+    #     BirthDate = request.json['BirthDate']
+    #     HireDate = request.json['HireDate']
+    #     Address = request.json['Address']
+    #     City = request.json['City']
+    #     State = request.json['State']
+    #     Country = request.json['Country']
+    #     PostalCode = request.json['PostalCode']
+    #     Phone = request.json['Phone']
+    #     Fax = request.json['Fax']
+    #     Email = request.json['Email']
+    #     query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
+    #                          '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
+    #                          '{13}')".format(LastName,FirstName,Title,
+    #                          ReportsTo, BirthDate, HireDate, Address,
+    #                          City, State, Country, PostalCode, Phone, Fax,
+    #                          Email))
+    #     return {'status':'success'}
 
     
 class Tracks(Resource):
@@ -63,4 +63,4 @@ api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
-     app.run()
+    app.run(debug=True)
