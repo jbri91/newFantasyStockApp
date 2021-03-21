70026918244de32327221d93cde0967a744bfbc9
Johnny Briones <jbrrii91@gmail.com>
2021-03-20 22:12:09 -0400

    deleted practice code

--- python_rest/server.py
+++ python_rest/server.py
@@ -1,66 +1,22 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from sqlalchemy import create_engine
-from json import dumps
+from sqlalchemy import SQLAlchemy
 
-db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
-SELECT * FROM user_credentials;
-SELECT * FROM purchased_stock;
-
-class Employees(Resource):
-    def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+def connect(user, password, db, host='postgres', port=5432):
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
     
-    # def post(self):
-    #     conn = db_connect.connect()
-    #     print(request.json)
-    #     LastName = request.json['LastName']
-    #     FirstName = request.json['FirstName']
-    #     Title = request.json['Title']
-    #     ReportsTo = request.json['ReportsTo']
-    #     BirthDate = request.json['BirthDate']
-    #     HireDate = request.json['HireDate']
-    #     Address = request.json['Address']
-    #     City = request.json['City']
-    #     State = request.json['State']
-    #     Country = request.json['Country']
-    #     PostalCode = request.json['PostalCode']
-    #     Phone = request.json['Phone']
-    #     Fax = request.json['Fax']
-    #     Email = request.json['Email']
-    #     query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-    #                          '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-    #                          '{13}')".format(LastName,FirstName,Title,
-    #                          ReportsTo, BirthDate, HireDate, Address,
-    #                          City, State, Country, PostalCode, Phone, Fax,
-    #                          Email))
-    #     return {'status':'success'}
+    con = SQLAlchemy.create_engine(url, client_encoding='utf8')
 
-    
-class Tracks(Resource):
-    def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
+    meta = SQLAlchemy.MetaData(bind=con, reflect=True)
 
-    
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
+    return con, meta
 
-
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+print(con)
+print(meta)
 
 
 if __name__ == '__main__':
