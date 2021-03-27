02fb02c68cd9027470875432100bfcd2ec5240cd
Johnny Briones <jbrrii91@gmail.com>
2021-03-27 15:48:29 -0400

    api

--- app.py
+++ app.py
@@ -11,20 +11,29 @@ api = Api(app)
 # Connecting with Trial Stock API
 stock = requests.get('https://sandbox.iexapis.com/stable/stock/IBM/quote?token=Tpk_b6429f1574564a01b54d614f88e0f93f').json()
 
-iexCloud = requests.get('https://cloud.iexapis.com/stable/')
+headers = {'Authorization': 'pk_75972e634de441d4a997ed43057a5221', 'Accept' : 'application.json', 'Content-Type' : 'application/json'}
+iexCloud = requests.get('https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10', headers=headers)
+print(iexCloud)
+
+class SearchStock(Resource):
+    def get(self):
+        headers = {'Authorization': 'pk_75972e634de441d4a997ed43057a5221', 'Accept' : 'application.json', 'Content-Type' : 'application/json'}
+        searchStock = requests.get('https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10', headers = headers).json()
+        print(searchStock)
+        return jsonify(searchStock)
+
+
 
 # Connecting with Database
 conn = psycopg2.connect(dbname='stock_application', user='postgres', password='databasePassword', host='localhost')
 cur = conn.cursor()
 # cur.execute('SELECT * FROM user_credentials;')
 cur.execute('SELECT * FROM purchased_stock;')
-print(cur.fetchall())
 purchasedStock = cur.fetchall()
 conn.commit()
 cur.close()
 conn.close()
 
-print(purchasedStock)
 
 class Stock(Resource):
     def get(self):
@@ -53,7 +62,7 @@ class UserCredentials(Resource):
         return jsonify(userCredentials)
         
 
-
+api.add_resource(SearchStock, '/searchStock')
 api.add_resource(UserCredentials, '/userCredentials')
 api.add_resource(Stock, '/stock')
 api.add_resource(PurchasedStock, '/purchased')
