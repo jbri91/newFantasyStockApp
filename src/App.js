868a77bcfd31183687446931933594946bc0a3b7
Johnny Briones <jbrrii91@gmail.com>
2021-04-25 22:26:27 -0400

    protected routes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,62 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationBar from "./components/NavigationBar";
 import HomePage from "./components/HomePage";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import ReportPage from "./components/ReportPage";
 import SummaryPage from "./components/SummaryPage";
 import CreateAccount from "./components/CreateAccount";
 
 function App() {
+
+
+  const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+      this.isAuthenticated = true;
+      setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+      this.isAuthenticated = false;
+      setTimeout(cb, 100); // fake async
+    },
+  };
+
+  fakeAuth.isAuthenticated = true;
+
+
+  function PrivateRoute({ children, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={() => {
+          return fakeAuth.isAuthenticated === true ? (
+            children
+          ) : (
+            <Redirect to="/" />
+          );
+        }}
+      />
+    );
+  }
+
   return (
     <div className="App">
-    <header className="App-header">
-         <BrowserRouter>
+      <header className="App-header">
+        <BrowserRouter>
           <NavigationBar />
           <Switch>
             <Route path="/" component={HomePage} exact />
-            <Route path="/report" component={ReportPage} />
-            <Route path="/summary" component={SummaryPage} />
-            <Route path='/createAccount' component={CreateAccount} />
+            <PrivateRoute path="/report">
+              <ReportPage />
+            </PrivateRoute>
+            <PrivateRoute path="/summary">
+              <SummaryPage />
+            </PrivateRoute>
+            <Route path="/createAccount" component={CreateAccount} />
           </Switch>
         </BrowserRouter>
-        </header>
-        </div>
+      </header>
+    </div>
   );
 }
 
