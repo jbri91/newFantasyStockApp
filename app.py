04465fe5d406d877108e6f5c7018b70fc28200ff
Johnny Briones <jbrrii91@gmail.com>
2021-06-29 00:46:38 -0400

    commit

--- app.py
+++ app.py
@@ -440,7 +440,7 @@ class UserAndPassword(Resource):
 api.add_resource(UserAndPassword, '/api/credentials')
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port=port)
 
 # app.run(debug=True, port = 5000)
