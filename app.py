ff6db9ff5b0198969367dfb5390529c97173cff4
Johnny Briones <jbrrii91@gmail.com>
2021-06-30 21:06:08 -0400

    home route

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
 SECRET_TOKEN = os.environ.get('SECRET_TOKEN')
 # DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
