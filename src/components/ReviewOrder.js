ce0c97e88c0a8b8f4748049da980476d012e8ecf
Johnny Briones <jbrrii91@gmail.com>
2021-07-04 14:32:44 -0400

    commented out the delete all row

--- src/components/ReviewOrder.js
+++ src/components/ReviewOrder.js
@@ -112,39 +112,40 @@ function ReviewOrder(props) {
             .then((data) => setBuyingPower(data))
             .catch((error) => console.log(error))
         );
-      } else {
-        console.log("I am getting called to delete the whole row");
-        fetch("/api/deleterow", {
-          method: "DELETE",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            stock_id: stockId,
-          }),
-        }).then(
-          fetch(`/api/purchased/${userId}`)
-            .then((res) => res.json())
-            .then((data) => props.setPurchasedStocks(data))
-            .catch((error) => console.log(error))
-        );
-        fetch("/api/boughtstock", {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            userId: parseInt(userId),
-            boughtStock: sellingStock,
-          }),
-        }).then(
-          fetch("/api/userbalance", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              userId: userId,
-            }),
-          })
-            .then((res) => res.json())
-            .then((data) => setBuyingPower(data))
-        );
-      }
+      } 
+      // else {
+      //   console.log("I am getting called to delete the whole row");
+      //   fetch("/api/deleterow", {
+      //     method: "DELETE",
+      //     headers: { "Content-Type": "application/json" },
+      //     body: JSON.stringify({
+      //       stock_id: stockId,
+      //     }),
+      //   }).then(
+      //     fetch(`/api/purchased/${userId}`)
+      //       .then((res) => res.json())
+      //       .then((data) => props.setPurchasedStocks(data))
+      //       .catch((error) => console.log(error))
+      //   );
+      //   fetch("/api/boughtstock", {
+      //     method: "PUT",
+      //     headers: { "Content-Type": "application/json" },
+      //     body: JSON.stringify({
+      //       userId: parseInt(userId),
+      //       boughtStock: sellingStock,
+      //     }),
+      //   }).then(
+      //     fetch("/api/userbalance", {
+      //       method: "POST",
+      //       headers: { "Content-Type": "application/json" },
+      //       body: JSON.stringify({
+      //         userId: userId,
+      //       }),
+      //     })
+      //       .then((res) => res.json())
+      //       .then((data) => setBuyingPower(data))
+      //   );
+      // }
     }
   }
 
