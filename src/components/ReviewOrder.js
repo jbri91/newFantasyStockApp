12311e180749701e357b27a7efb485f70c714899
Johnny Briones <jbrrii91@gmail.com>
2021-07-04 21:23:59 -0400

    commit

--- src/components/ReviewOrder.js
+++ src/components/ReviewOrder.js
@@ -24,7 +24,7 @@ function ReviewOrder(props) {
         .then((data) => setPurchasedStocks(data))
         .catch((error) => console.log(error));
     }
-  }, [shares, stockId]);
+  }, []);
 
   function handlePlaceOrder() {
     let boughtStock = buyingPower - props.stockSum;
