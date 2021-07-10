d29c9ed7319c115a1384cec1669ba02778882448
Johnny Briones <jbrrii91@gmail.com>
2021-07-09 22:26:19 -0400

    removed second app.run

--- app.py
+++ app.py
@@ -491,4 +491,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', debug=True, port=port)
 
-app.run(debug=True, port = 5000)
+# app.run(debug=True, port = 5000)
