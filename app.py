54648da5ab97730c9b1864467b9faac026c2ee8d
Johnny Briones <jbrrii91@gmail.com>
2021-10-07 07:28:52 -0400

    switched port in app.py to 5000 from 3000

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ api.add_resource(UserAndPassword, '/api/credentials')
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', debug=True, port=port)
 
 # app.run(debug=True, port = 5000)
